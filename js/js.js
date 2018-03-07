a49c02fedd8ec5f88131757fa96f66922a55ee61
ach74 <32370243+ach74@users.noreply.github.com>
2018-03-07 02:11:00 +0100

    ach
    Minimificada

--- js/js.js
+++ js/js.js
@@ -1,65 +1,10 @@
-window.onload = function(){
-
-	document.getElementById("BotonHome").onclick=function(){
-		MostrarHome();
-	}
-
-	document.getElementById("BotonCircuito").onclick=function(){
-		MostrarCircuito();
-	}
-
-	document.getElementById("BotonQuedadas").onclick=function(){
-		MostrarQuedadas();
-	}
-
-	document.getElementById("BotonGaleria").onclick=function(){
-		MostrarGaleria();
-	}
-
-	document.getElementById("BotonAcerca").onclick=function(){
-		MostrarAcerca();
-	}
-
-}
-
-function MostrarHome(){
-	document.getElementById("home").style.display="block";
-	document.getElementById("circuito").style.display="none";
-	document.getElementById("quedadas").style.display="none";
-	document.getElementById("galeria").style.display="none";
-	document.getElementById("acerca").style.display="none";
-}
-
-function MostrarCircuito(){
-	document.getElementById("circuito").style.display="block";
-	document.getElementById("home").style.display="none";
-	document.getElementById("quedadas").style.display="none";
-	document.getElementById("galeria").style.display="none";
-	document.getElementById("acerca").style.display="none";
-}
-
-function MostrarQuedadas(){
-	document.getElementById("quedadas").style.display="block";
-	document.getElementById("circuito").style.display="none";
-	document.getElementById("home").style.display="none";
-	document.getElementById("galeria").style.display="none";
-	document.getElementById("localizacion").style.display="none";
-	document.getElementById("acerca").style.display="none";
-}
-
-function MostrarGaleria(){
-	document.getElementById("galeria").style.display="block";
-	document.getElementById("home").style.display="none";
-	document.getElementById("quedadas").style.display="none";
-	document.getElementById("circuito").style.display="none";
-	document.getElementById("acerca").style.display="none";
-}
-
-function MostrarAcerca(){
-	document.getElementById("acerca").style.display="block";
-	document.getElementById("home").style.display="none";
-	document.getElementById("quedadas").style.display="none";
-	document.getElementById("galeria").style.display="none";
-	document.getElementById("circuito").style.display="none";
-}
-
+window.onload=function(){document.getElementById("BotonHome").onclick=function(){MostrarHome();}
+document.getElementById("BotonCircuito").onclick=function(){MostrarCircuito();}
+document.getElementById("BotonQuedadas").onclick=function(){MostrarQuedadas();}
+document.getElementById("BotonGaleria").onclick=function(){MostrarGaleria();}
+document.getElementById("BotonAcerca").onclick=function(){MostrarAcerca();}}
+function MostrarHome(){document.getElementById("home").style.display="block";document.getElementById("circuito").style.display="none";document.getElementById("quedadas").style.display="none";document.getElementById("galeria").style.display="none";document.getElementById("acerca").style.display="none";}
+function MostrarCircuito(){document.getElementById("circuito").style.display="block";document.getElementById("home").style.display="none";document.getElementById("quedadas").style.display="none";document.getElementById("galeria").style.display="none";document.getElementById("acerca").style.display="none";}
+function MostrarQuedadas(){document.getElementById("quedadas").style.display="block";document.getElementById("circuito").style.display="none";document.getElementById("home").style.display="none";document.getElementById("galeria").style.display="none";document.getElementById("localizacion").style.display="none";document.getElementById("acerca").style.display="none";}
+function MostrarGaleria(){document.getElementById("galeria").style.display="block";document.getElementById("home").style.display="none";document.getElementById("quedadas").style.display="none";document.getElementById("circuito").style.display="none";document.getElementById("acerca").style.display="none";}
+function MostrarAcerca(){document.getElementById("acerca").style.display="block";document.getElementById("home").style.display="none";document.getElementById("quedadas").style.display="none";document.getElementById("galeria").style.display="none";document.getElementById("circuito").style.display="none";}
